ea58a0151adfc5bf9cc6a42515db0b4c41cfe4f4
lxb-blog <97530887+lxb-blog@users.noreply.github.com>
2025-03-16 21:50:06 +0800

    Create body.js

---
+++ data/body.js
@@ -0,0 +1,17 @@
+{
+      "uuid": "b3bb24c7-a1cf-47e2-af8a-aadde06234d8",
+      "metadata_covered": false,
+      "metadata_skipped": false,
+      "task_name": "alist",
+      "media_dir": "/netdisk/alist/夸克/来自：分享",
+      "symlink_creator": true,
+      "metadata_copyer": true,
+      "metadata_copyer_mode": "下载模式",
+      "num_threads": "2",
+      "symlink_dir_checker": true,
+      "symlink_checker": true,
+      "metadata_checker": true,
+      "symlink_size": "0",
+      "symlink_ext": ".mkv;.iso;.ts;.mp4;.avi;.rmvb;.wmv;.m2ts;.mpg;.flv;.rm;.mov",
+      "metadata_ext": ".nfo;.jpg;jpeg;.png;.svg;.ass;.srt;.sup;.mp3;.flac;.wav;.aac"
+    }
